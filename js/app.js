42e70317c9d431e87b4c37c4f0cef852aa8c2e23
mario <icutum@hotmail.com>
2022-08-16 03:07:22 +0200

    Added resize function to cursor when hovering links

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
+const cursor = document.querySelector('.cursor');
+
 trackMouse();
 function trackMouse() {
-    const cursor = document.querySelector('.cursor');
-
     addEventListener("mousemove", e => {
         cursor.style.transform = `translate(-50%, -50%) translate(${e.clientX}px, ${e.clientY}px)`;
     });
+}
+
+resizeCursor();
+function resizeCursor() {
+    const links = document.getElementsByTagName("a");
+    
+    for (link of links) {
+        link.addEventListener("mouseenter", () => {
+            cursor.classList.add("cursor--big");
+        });
+
+        link.addEventListener("mouseleave", () => {
+            cursor.classList.remove("cursor--big");
+        });
+    }
 }
