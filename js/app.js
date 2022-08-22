26485e0a4772dcf04dbf0067899b8d1be6fc1e51
mario <icutum@hotmail.com>
2022-08-22 15:14:08 +0200

    Added typewriter animation function

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const cursor = document.querySelector('.cursor');
+const cursor = document.querySelector(".cursor");
 
 trackMouse();
 function trackMouse() {
@@ -19,5 +19,45 @@ function resizeCursor() {
         link.addEventListener("mouseleave", () => {
             cursor.classList.remove("cursor--big");
         });
+    }
+}
+
+descriptionTyping();
+function descriptionTyping() {
+    const description = document.querySelector(".hero__description");const descriptions = ["desarrollador web", "procrastinador 24/7"];
+
+    const typeDelay = 175;
+    const deleteDelay = 50;
+
+    let descriptionsIndex = 0;
+    let charIndex = 0;
+
+    typeText();
+    function typeText() {
+        if (charIndex < descriptions[descriptionsIndex].length) {
+            description.innerHTML += descriptions[descriptionsIndex].charAt(charIndex);
+
+            charIndex++;
+
+            setTimeout(typeText, typeDelay);
+        } else {
+            setTimeout(deleteText, deleteDelay);
+        }
+    }
+
+    function deleteText() {
+        if (charIndex > 0) {
+            description.innerHTML = descriptions[descriptionsIndex].substring(0, charIndex - 1);
+
+            charIndex--;
+
+            setTimeout(deleteText, deleteDelay);
+        } else {
+            descriptionsIndex++;
+
+            if (descriptionsIndex >= descriptions.length)descriptionsIndex = 0;
+
+            setTimeout(typeText, typeDelay);
+        }
     }
 }
